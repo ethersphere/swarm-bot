7bca37fdb692795ad995262437e42ac53d021e6e
Philippe Schommers <philippe@schommers.be>
2021-05-19 15:26:51 +0200

    WIP: Use faucet contract

---
+++ jobs/fund/index.js
@@ -0,0 +1,127 @@
+const config = require("config");
+const { subMilliseconds } = require("date-fns");
+const { getDefaultProvider, Wallet, Contract, utils } = require("ethers");
+const abi = require("./abi.json");
+
+// Redis keys
+const QUEUE_KEY = "spinkles:queue";
+const addressKey = (address) => `address:${address}:sprinkle`;
+const sprinklesKey = (user) => `user:${user}:sprinkles`;
+
+// Functions
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const promiseTimeout = (promise, ms) => {
+  const timeout = new Promise((_, reject) => {
+    const id = setTimeout(() => {
+      clearTimeout(id);
+      reject({ code: "TIMEOUT" });
+    }, ms);
+  });
+
+  return Promise.race([promise, timeout]);
+};
+
+// Ethers setup
+const provider = getDefaultProvider(config.get("ethereum.endpoint"));
+const wallet = new Wallet(config.get("ethereum.privateKey"), provider);
+const faucet = new Contract(config.get("contracts.faucet"), abi, wallet);
+
+const create = async ({ redis, discord }) => {
+  let stop = false;
+  const eventTopic = faucet.filters.Funded().topics[0];
+
+  const fund = async (attempt = 0) => {
+    const addresses = await redis.lrange(
+      QUEUE_KEY,
+      0,
+      config.get("jobs.fund.maxBatch")
+    );
+    if (!addresses.length) {
+      return;
+    }
+
+    console.log("Fetching gas price...");
+    const gasPrice = (await provider.getGasPrice()).add(
+      utils.parseUnits((2 * attempt).toString(), "gwei")
+    );
+
+    try {
+      const tx = await faucet.fund(addresses, { gasPrice });
+      console.log(
+        `Bulk funding ${addresses.length} addresses at ${tx.hash} (${gasPrice} gwei)`
+      );
+
+      const result = await promiseTimeout(tx.wait(), 30 * 1000);
+      const success = {};
+      for (const event of result.events) {
+        if (event.topics.includes(eventTopic)) {
+          success[event.args.addr] = true;
+        }
+      }
+
+      const results = await Promise.all(
+        addresses.map(async (address) => ({
+          success: !!success[address],
+          address,
+          ...(await redis.hgetall(addressKey(address))),
+        }))
+      );
+
+      await redis.lpop(QUEUE_KEY, addresses.length);
+      await results.map((result) =>
+        redis.sadd(sprinklesKey(result.user), result.address)
+      );
+
+      for (const result of results) {
+        const channel = discord.guilds.cache
+          .get(result.guild)
+          .channels.cache.get(config.get("faucet.channel"));
+
+        channel.send(
+          `<@${result.user}> Your node ${result.address} was sprinkled! :bee:`
+        );
+      }
+    } catch (err) {
+      if ([-32010, "TIMEOUT"].includes(err.code)) {
+        console.log("Transaction timeout or exists");
+        return await fund(attempt + 1);
+      }
+
+      console.error(err);
+    }
+  };
+
+  const start = async () => {
+    let lastFund = 0;
+    while (!stop) {
+      const queue = await redis.llen(QUEUE_KEY);
+      const time = subMilliseconds(
+        new Date(),
+        config.get("jobs.fund.maxFundInterval")
+      );
+
+      if (queue >= config.get("jobs.fund.minQueue") || lastFund < time) {
+        try {
+          await fund();
+        } catch (err) {
+          console.error(err);
+        }
+        lastFund = new Date();
+      }
+
+      // Always add a small delay between funds or checks
+      await sleep(config.get("jobs.fund.checkInterval"));
+    }
+  };
+
+  return {
+    start,
+    stop: () => {
+      stop = true;
+    },
+  };
+};
+
+module.exports = {
+  create,
+};
